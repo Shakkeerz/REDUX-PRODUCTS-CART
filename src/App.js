3c93e95b827588148d633680a5df25a65e34810a
shakkeerz <shakkeerpersonal@gmail.com>
2022-10-17 09:33:09 -0700

    last commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Header from "./components/Header";
-import ProductList from "./components/ProductList";
 function App() {
   return (
   <>
   <div className="p-5 bg-slate-800 text-white">
 
-    <Header/>
-    <ProductList/>
+    Muhammad shakkeer
   </div>
   </>
   );
